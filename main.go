53104a4ca660a0e54cb013d13e3cbf5542f7c356
Tom Thorogood <me+github@tomthorogood.co.uk>
2021-04-12 20:15:24 +0930

    Revert "Use different domains for redirects"
    This reverts commit 691f43681b9d634a3957c438ed077d310604f930.

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	crand "crypto/rand"
-	"encoding/binary"
 	"fmt"
-	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
@@ -12,10 +9,6 @@ import (
 )
 
 func main() {
-	var seed int64
-	binary.Read(crand.Reader, binary.LittleEndian, &seed)
-	rand.Seed(seed)
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8090"
@@ -26,9 +19,5 @@ func main() {
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	i, _ := strconv.ParseInt(strings.TrimPrefix(r.URL.Path, "/"), 10, 64)
-	to, _ := r.URL.Parse(fmt.Sprintf("/%d", i+1))
-	to.Scheme = "https"
-	to.Host = fmt.Sprintf("%x.badredirect.tmthrgd.dev", rand.Intn(16))
-	w.Header().Set("Connection", "close")
-	http.Redirect(w, r, to.String(), http.StatusTemporaryRedirect)
+	http.Redirect(w, r, fmt.Sprintf("/%d", i+1), http.StatusTemporaryRedirect)
 }
